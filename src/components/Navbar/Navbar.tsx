2829d9c393669a67668c6904eb63c81fe58d983f
Mert Şakar <mertsakar@hotmail.com>
2023-02-07 22:48:42 +0300

    Navbar component basic structure created

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,18 @@
+import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
 
 const Navbar: React.FC = () => {
 
-    return <div>Navbar</div>
+    return (
+        <Flex bg="white" height="44px" padding="6px 12px">
+            <Flex align="center">
+                <Image src="/images/redditFace.svg" alt="reddit logo" height="30px" />
+                <Image src="/images/redditText.svg" alt="reddit logo text" height="46px" display={{ base: "none", md: "unset" }} />
+            </Flex>
+            {/* <Directory />
+            <SearchInput />
+            <RightContent /> */}
+        </Flex>
+    )
 }
 export default Navbar;
