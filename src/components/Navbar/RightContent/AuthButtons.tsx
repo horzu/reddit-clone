9ab4cc7d0d5166eee27119222a5e8fbca21af8fb
Mert Şakar <mertsakar@hotmail.com>
2023-02-12 14:58:58 +0300

    Added auth modal

--- src/components/Navbar/RightContent/AuthButtons.tsx
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -1,8 +1,10 @@
+import { authModalState } from '@/src/atoms/authModalAtom';
 import { Button } from '@chakra-ui/react';
 import React from 'react';
+import { useSetRecoilState } from 'recoil';
 
 const AuthButtons: React.FC = () => {
-
+    const setAuthModalState = useSetRecoilState(authModalState);
     return (
         <>
             <Button variant="outline"
@@ -10,13 +12,15 @@ const AuthButtons: React.FC = () => {
                 display={{ base: "none", sm: "flex" }}
                 width={{ base: "70px", md: "110px" }}
                 mr={2}
-            // onClick={() => { }}
+                onClick={() => setAuthModalState({ open: true, view: "login" })}
             >Login</Button>
             <Button
                 height="28px"
                 display={{ base: "none", sm: "flex" }}
                 width={{ base: "70px", md: "110px" }}
-                mr={2}>Sign Up</Button>
+                mr={2}
+                onClick={() => setAuthModalState({ open: true, view: "signup" })}
+            >Sign Up</Button>
         </>
     )
 }
