08c25bcf99ecf477207a4be107abc0d6526ebb9a
Mert Şakar <mertsakar@hotmail.com>
2023-02-24 15:58:23 +0300

    keep auth state - login, logout

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,21 @@
 import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import RightContent from './RightContent/RightContent';
 import SearchInput from './SearchInput';
+import { auth } from "../../firebase/clientApp"
 
 const Navbar: React.FC = () => {
-
+    const [user, loading, error] = useAuthState(auth);
     return (
         <Flex bg="white" height="44px" padding="6px 12px">
             <Flex align="center">
                 <Image src="/images/redditFace.svg" alt="reddit logo" height="30px" />
                 <Image src="/images/redditText.svg" alt="reddit logo text" height="46px" display={{ base: "none", md: "unset" }} />
             </Flex>
+            {/* <Directory />*/}
             <SearchInput />
-            <RightContent />
-            {/* <Directory />
-             */}
+            <RightContent user={user} />
         </Flex>
     )
 }
