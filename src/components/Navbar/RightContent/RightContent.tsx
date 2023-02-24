c397abf29d423f684dd4e0ca68b4b1370384f002
Mert Şakar <mertsakar@hotmail.com>
2023-02-24 23:19:36 +0300

    user dropdown menu and karma on navbar

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -5,6 +5,7 @@ import AuthModal from '../../Modal/Auth/AuthModal';
 import AuthButtons from './AuthButtons';
 import { auth } from "../../../firebase/clientApp"
 import Icons from './Icons';
+import UserMenu from './UserMenu';
 
 type RightContentProps = {
     user?: User | null
@@ -17,6 +18,7 @@ const RightContent: React.FC<RightContentProps> = ({ user }) => {
             <AuthModal />
             <Flex justify="center" align="center">
                 {user ? (<Icons />) : <AuthButtons />}
+                <UserMenu user={user} />
             </Flex>
         </>
     )
