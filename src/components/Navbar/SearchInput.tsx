9d2722da975f7faea5b066e96051279bbbf06d4c
Mert Şakar <mertsakar@hotmail.com>
2023-02-24 23:46:48 +0300

    community directory in navbar

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -1,20 +1,22 @@
 import { SearchIcon } from '@chakra-ui/icons';
 import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { User } from '@firebase/auth';
 import React from 'react';
 
 type SearchInputProps = {
-    // user: 
+    user?: User | null;
 };
 
-const SearchInput: React.FC<SearchInputProps> = () => {
+const SearchInput: React.FC<SearchInputProps> = ({ user }) => {
 
     return (
-        <Flex flexGrow={1} mr={2} align="center">
+        <Flex flexGrow={1} maxWidth={user ? "auto" : "600px"} mr={2} align="center">
             <InputGroup>
                 <InputLeftElement
                     pointerEvents='none'
-                    children={<SearchIcon color='gray.400' mb={1} />}
-                />
+                >
+                    <SearchIcon color='gray.400' mb={1} />
+                </InputLeftElement>
                 <Input placeholder='Search Reddit' fontSize="10pt" _placeholder={{ color: "gray.500" }} _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }} _focus={{ outline: "none", border: "1px solid", borderColor: "blue.500" }} height="34px" bg="gray.50" />
             </InputGroup>
         </Flex>
