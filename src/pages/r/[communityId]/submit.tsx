a097ef36724e4f9e70a1374520ea6aed5db75603
Mert Şakar <mertsakar@hotmail.com>
2023-03-05 15:04:32 +0300

    NewPostFrom: FormTabs component

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,7 +1,22 @@
+import PageContent from '@/src/components/Layout/PageContent';
+import NewPostForm from '@/src/components/Posts/NewPostForm';
+import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
 
 const SubmitPostPage: React.FC = () => {
 
-    return <div>Submit Page</div>
+    return (
+        <PageContent>
+            <>
+                <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
+                    <Text>Create a Post</Text>
+                </Box>
+                <NewPostForm />
+            </>
+            <>
+                {/* About  */}
+            </>
+        </PageContent>
+    )
 }
 export default SubmitPostPage;
