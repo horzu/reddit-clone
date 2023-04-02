ad67ecce41c8e3b58a9a673bbd7fb94cf44bc644
Mert Şakar <mertsakar@hotmail.com>
2023-04-02 21:57:57 +0300

    deployment fix TabItemComponent

--- src/components/Posts/TabItem.tsx
+++ src/components/Posts/TabItem.tsx
@@ -1,9 +1,9 @@
 import { Flex, Icon, Text } from '@chakra-ui/react';
 import React from 'react';
-import { TabItem } from "./NewPostForm";
+import { TabItemComponent } from "./NewPostForm";
 
 type TabItemProps = {
-	item: TabItem;
+	item: TabItemComponent;
 	selected: boolean;
 	setSelectedTab: (value: string) => void;
 };
